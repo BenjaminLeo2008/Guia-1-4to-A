7e1029196c3e0e0a2479052e873392f3871b2acd
BenjaminLeo2008 <166539016+BenjaminLeo2008@users.noreply.github.com>
2025-04-01 11:39:03 -0300

    ejercicio 3 terminado

---
+++ Assets/Scripts/Calculos.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculos : MonoBehaviour
+{
+    public float num1 = 10f;
+    public float num2 = 32f;
+    public float resultado;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resultado = num1 + num2;
+        Debug.Log(resultado);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
